f7790a312aa218ed2705c922bd6aa0d4405d4b17
viastolfi <vincent.astolfi12@gmail.com>
2025-12-10 23:38:58 +0100

    feat: read applications from /usr dir

--- src/main.c
+++ src/main.c
@@ -1,10 +1,55 @@
+#include <dirent.h>
+#include <stdio.h>
+#include <string.h>
+
 #include "raylib.h"
 
 #define RAYGUI_IMPLEMENTATION
 #include "thirdparty/raygui.h"
 
+#define DA_LIB_IMPLEMENTATION
+#include "thirdparty/da.h"
+
+typedef struct {
+  char** items;
+  size_t count;
+  size_t capacity;
+} files_t;
+
+int get_applications_from_dir(const char* path, files_t* fda)
+{
+  DIR *dir;
+  struct dirent *ent;
+  if ((dir = opendir (path)) != NULL) {
+    while ((ent = readdir (dir)) != NULL) {
+      if (strcmp(ent->d_name, "..") == 0 ||
+          strcmp(ent->d_name, ".") == 0)
+        continue;
+      da_append(fda, ent->d_name);
+    }
+    closedir (dir);
+  } else {
+    return 0;
+  }
+
+  return 1;
+}
+
 int main()
 {
+  files_t applications = {0};
+
+  if (!get_applications_from_dir("/usr/share/applications", &applications)) {
+    printf("error while reading applications file\n");
+    return 1; 
+  }
+
+  /*
+  da_foreach(char*, it, &applications) {
+    printf("%s\n", (*it)); 
+  }
+  */
+
   InitWindow(400, 200, "raygui - controls test suite");
   SetTargetFPS(60);
 
